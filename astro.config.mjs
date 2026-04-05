da96092a0ab8562696a3a41ea9d3b03f5c8ac4e4
ThisRaihann <mr.fewsx212@gmail.com>
2026-04-05 18:29:05 +0700

    seo1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  // WAJIB: Ganti dengan domain asli lu nanti pas sudah deploy
+  site: 'https://ashel-quran.vercel.app/', 
+
   vite: {
     plugins: [tailwindcss()]
   },
 
+  // Integrasi sitemap otomatis bikin peta situs buat Google
   integrations: [sitemap()]
 });
